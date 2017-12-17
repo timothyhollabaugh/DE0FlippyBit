a38d58a6dd9bb4ceb54d3419833d99f42fea2aff
Tim <timothyhollabaugh@gmail.com>
2017-12-17 13:18:57 -0500

    Clean up top level entity some

--- Top_Level_Entity/Flippin_Bits_Man_Project.v
+++ Top_Level_Entity/Flippin_Bits_Man_Project.v
@@ -1,15 +1,15 @@
 
-module Flippy_Bit (CLOCK_50, SW, LEDG, BUTTON, HEX0, HEX1, HEX2, HEX3); 
+module Flippy_Bit (CLOCK_50, SW, LEDG, BUTTON, HEX0, HEX1, HEX2, HEX3, VGA_R, VGA_G, VGA_HS, VGA_VS); 
     input CLOCK_50;
     input [7:0] SW;
     input [2:0] BUTTON;
     output [6:0] HEX0, HEX1, HEX2, HEX3;
-    
-    wire [2:0]    state, next_state, game_over, correct;
+
+    wire [2:0]    game_over, correct;
     wire [1199:0] framebuffer;
     wire [4:0]    ypos1, ypos2, ypos3;
     wire [7:0]    score, letter1, letter2, letter3;
-    
+
     wire [6:0] seven_seg_hex0;
     wire [6:0] seven_seg_hex1;
     wire [6:0] ones_hex2;
@@ -17,20 +17,51 @@ module Flippy_Bit (CLOCK_50, SW, LEDG, BUTTON, HEX0, HEX1, HEX2, HEX3);
     wire [3:0] ones;
     wire [3:0] tens;
     wire [3:0] hundreds;
-    
-    wire [3:0] red_out, green_out, blue_out; //From VGA; Change Accordingly!
-    wire h_sync_out, v_sync_out, out; //From VGA; Change Accordingly!
 
-    wire clock_some_hertz; //Change when hertz value is decided
-    wire reset_button, reset_signal; 
-    
+    wire reset_signal;
+
     assign HEX2_DP = 1'b0;
     assign reset_button = BUTTON[2];
-    
-    Big_State_Machine big_state_machine (reset_button, game_over, correct, reset_signal, score, clock_some_hertz);
-    
-    Column column1 (CLOCK_50, user_input, reset_signal, ypos, game_over, correct, letter); // change as needed
-    
+
+    Big_State_Machine big_state_machine (
+        .reset_button(~BUTTON[2]),
+        .game_over(game_over),
+        .correct(correct),
+        .reset_signal(reset_signal),
+        .score(score),
+        .clock(CLOCK_50)
+    );
+
+    Column column1 (
+        .clock(CLOCK_50),
+        .user_input(SW[7:0],
+        .reset_signal(reset_signal),
+        .ypos(ypos1),
+        .game_over(game_over[0],
+        .correct(correct[0],
+        .letter(letter1)
+    );
+
+    Display display (
+        .clock(CLOCK_50),
+        .letter1(letter1),
+        .letter2(letter2),
+        .letter3(letter3),
+        .ypos1(ypos1),
+        .ypos2(ypos2),
+        .ypos3(ypos3),
+        .framebuffer(framebuffer);
+    );
+
+    VGA vga (
+        .clk(CLOCK_50),
+        .data(framebuffer),
+        .red_out(VGA_R),
+        .green_out(VGA_G),
+        .h_sync_out(VGA_HS),
+        .v_sync_out(VGA_VS),
+    );
+
     assign LEDG = {tens, ones};
     assign HEX0 = ~seven_seg_hex0;
     assign HEX1 = ~seven_seg_hex1;
@@ -42,10 +73,7 @@ module Flippy_Bit (CLOCK_50, SW, LEDG, BUTTON, HEX0, HEX1, HEX2, HEX3);
     Seven_Seg hex2  (ones, ones_hex2);
     Seven_Seg hex3  (tens, tens_hex3);
     Binary_BCD one_hex2 (score, ones, tens, hundreds);
-    
-    Display display (CLOCK_50, letter1, letter2, letter3, ypos1, ypos2, ypos3, framebuffer);
-    
-    VGA vga (CLOCK_50, framebuffer, );
-    
+
+
 endmodule
-    
+
